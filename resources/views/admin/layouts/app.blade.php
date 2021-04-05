cb43e8cd6aeac553ebd238df5ba90ce27a219bd4
Warithar Kamvilai <warithar@netway.co.th>
2021-04-05 09:47:00 +0000

    Change indent tab to 4 space.

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('leftmenu')
-	@include('admin.includes.leftmenu', ['package_name' => "rvsitebuilder/laravelsitemap"])
+    @include('admin.includes.leftmenu', ['package_name' => "rvsitebuilder/laravelsitemap"])
 @endsection
 
 @push('package-styles')
@@ -14,7 +14,7 @@
 @push('package-scripts')
     <!-- package-scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" 
+    integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s="
     crossorigin="anonymous"></script>
 @endpush
 
