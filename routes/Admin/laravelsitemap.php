80d174d0933bdebac5e9b15fb70069b410002044
Apiruk Boonruksa <apiruk@netway.co.th>
2021-09-01 09:18:28 +0000

    Update: Coding Standard laravelsitemap app

--- routes/Admin/laravelsitemap.php
+++ routes/Admin/laravelsitemap.php
@@ -4,33 +4,33 @@ use Rvsitebuilder\Laravelsitemap\Http\Controllers\Admin\LaravelSitemapController
 use Rvsitebuilder\Laravelsitemap\Http\Controllers\Admin\SettingController;
 
 Route::group([
-  'prefix' => 'admin',
-  'as' => 'admin.',
-  'middleware' => 'web',
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'middleware' => 'web',
 ], function () {
-  Route::group([
-    'prefix' => 'laravelsitemap',
-    'as' => 'laravelsitemap.',
-    'middleware' => 'admin',
-  ], function () {
     Route::group([
-      'prefix' => 'laravelsitemap',
-      'as' => 'laravelsitemap.',
+        'prefix' => 'laravelsitemap',
+        'as' => 'laravelsitemap.',
+        'middleware' => 'admin',
     ], function () {
-      // 'admin.laravelsitemap.laravelsitemap.'
-      Route::get('/', [LaravelSitemapController::class, 'index'])->name('index');
-      Route::get('/generate', [LaravelSitemapController::class, 'generate'])->name('generate');
-      Route::get('/download', [LaravelSitemapController::class, 'download'])->name('download');
-    });
+        Route::group([
+            'prefix' => 'laravelsitemap',
+            'as' => 'laravelsitemap.',
+        ], function () {
+            // 'admin.laravelsitemap.laravelsitemap.'
+            Route::get('/', [LaravelSitemapController::class, 'index'])->name('index');
+            Route::get('/generate', [LaravelSitemapController::class, 'generate'])->name('generate');
+            Route::get('/download', [LaravelSitemapController::class, 'download'])->name('download');
+        });
 
-    Route::group([
-      'prefix' => 'setting',
-      'as' => 'setting.',
-    ], function () {
-      // 'admin.laravelsitemap.setting.'
+        Route::group([
+            'prefix' => 'setting',
+            'as' => 'setting.',
+        ], function () {
+            // 'admin.laravelsitemap.setting.'
 
-      Route::get('/', [SettingController::class, 'index'])->name('index');
-      Route::post('/savesetting', [SettingController::class, 'savesetting'])->name('savesetting');
+            Route::get('/', [SettingController::class, 'index'])->name('index');
+            Route::post('/savesetting', [SettingController::class, 'savesetting'])->name('savesetting');
+        });
     });
-  });
 });
